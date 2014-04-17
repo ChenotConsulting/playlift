ecbffcaf7cce5c88b98a60f362f30b6b0630cb88
Jean-Philippe Chenot <jeanphilippe@totalresolutionmusic.com>
2014-04-17 18:53:36 +0100

    Initial commit of full application repository.

---
+++ TRMWebHttpCors/CorsWebServiceHostFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace TRMWebHttpCors
+{    
+    public class CorsWebServiceHostFactory : WebServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            var host = base.CreateServiceHost(serviceType, baseAddresses);
+            host.Opening += new EventHandler(host_Opening);
+            return host;
+        }
+
+        void host_Opening(object sender, EventArgs e)
+        {
+            var endpoints = ((ServiceHost) sender).Description.Endpoints.Where(se => se.Binding is WebHttpBinding);
+
+            foreach (var endpoint in endpoints)
+            {
+                // Add support for cross-origin resource sharing
+                endpoint.Behaviors.Add(new CorsSupportBehavior());
+            }
+        }
+    }
+}
---
+++ TRMWebTestRig/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRMWebTestRig.MvcApplication" Language="C#" %>
