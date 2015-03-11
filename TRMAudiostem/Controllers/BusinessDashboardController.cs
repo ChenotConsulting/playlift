3b51b1ea3ea71e2b416f045d9663027a2be33e92
trmltd <jeanphilippe@totalresolutionmusic.com>
2015-03-11 21:32:18 +0000

    Updated the business profile page to prepare for the dashboard tables.

---
+++ TRMAudiostem/Controllers/BusinessDashboardController.cs
@@ -0,0 +1,30 @@
+﻿using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRMAudiostem.Controllers
+{
+    public class BusinessDashboardController : Controller
+    {
+        //
+        // GET: /BusinessDashboard/
+
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult _Artists()
+        {
+            return PartialView(new List<Artist>());
+        }
+
+        public PartialViewResult _Activity()
+        {
+            return PartialView();
+        }
+    }
+}
