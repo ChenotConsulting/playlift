2896a1c44ef07fcf15fd9893be7a135f6876f96f
trmltd <jeanphilippe@totalresolutionmusic.com>
2015-03-11 20:25:20 +0000

    Added selected navigation items color scheme within the artist dashboard so that it is obvious when a page is selected. Deliberately skipped the main navigation as all pages concerned have a clear title that shows the user where they are.

---
+++ TRMAudiostem/Content/themes/trm/scripts/navigation.js
@@ -0,0 +1,7 @@
+﻿
+$(document).ready(function () {
+    $("ul.navigation > li > a").bind("click", function () {
+        $("ul.navigation > li > a").removeClass('selected');
+        $(this).addClass('selected');
+    });    
+});
